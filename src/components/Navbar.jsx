6b12d54b5d9b122364fb2bcda2617df1cee22b36
X.P <a1679190@gmail.com>
2025-01-01 20:52:39 +0800

    使用 imagekit 进行图片 CDN 加速

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { IKImage } from 'imagekitio-react';
+import Image from './Image';
 
 const Navbar = () =>{
     const [open , setOpen] = useState(false);
@@ -6,7 +8,7 @@ const Navbar = () =>{
         <div className='w-full h-16 md:h-20 flex items-center justify-between'>
             {/* LOGO */}
             <div className="flex items-center gap-4 text-2xl font-bold">
-                <img src="/logo.png" className="w-8 h-8" alt="" />
+                <Image src="logo.png" alt="navbar logo" w={32} h={32} />
                 <span>guda blog</span>
             </div>
             {/* MOBILE MENU */}
