a6f2bdc1277cf185d81ce4d683487e77742bbb3c
X.P <a1679190@gmail.com>
2025-01-06 15:09:54 +0800

    在首页添加文章列表组件，并优化布局

--- src/routers/Homepage.jsx
+++ src/routers/Homepage.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import MainCategories from "../components/MainCategories";
 import FeaturedPosts from "../components/FeaturedPosts";
+import PostLists from "../components/PostLists";
 const Homepage = () => {
   return (
     <div className="mt-4 flex flex-col gap-4">
       {/* BreadCrumb */}
       <div className="flex gap-4">
-        <Link to="/" >首页</Link>
+        <Link to="/">首页</Link>
         <span className="font-bold">·</span>
         <span className="text-blue-800">博客和文章</span>
       </div>
@@ -14,15 +15,16 @@ const Homepage = () => {
       <div className="flex items-center justify-between">
         {/* 标题 */}
         <div>
-          <h1 className="text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold">Lorem ipsum dolor, sit amet consectetur</h1>
+          <h1 className="text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold">
+            Lorem ipsum dolor, sit amet consectetur
+          </h1>
           <p className="mt-8 text-md md:text-lg">
-            Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Dignissimos ut eligendi 
-            ducimus! Et ex dolorum perspiciatis
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            ut eligendi ducimus! Et ex dolorum perspiciatis
           </p>
         </div>
         {/* 动画按钮 */}
-        <Link to='write' className="hidden md:block relative">
+        <Link to="write" className="hidden md:block relative">
           <svg
             viewBox="0 0 200 200"
             width="200"
@@ -35,8 +37,12 @@ const Homepage = () => {
               d="m 100,100 m -75,0 a 75,75 0 1 1 150,0 a 75,75 0 1 1 -150,0"
             />
             <text>
-              <textPath href="#circlePath" startOffset="0%">分享你的故事·</textPath>
-              <textPath href="#circlePath" startOffset="50%">书写你的想法·</textPath>
+              <textPath href="#circlePath" startOffset="0%">
+                分享你的故事·
+              </textPath>
+              <textPath href="#circlePath" startOffset="50%">
+                书写你的想法·
+              </textPath>
             </text>
           </svg>
           <button className="absolute top-0 left-0 right-0 bottom-0 m-auto w-20 h-20 bg-blue-800 rounded-full flex items-center justify-center">
@@ -50,17 +56,21 @@ const Homepage = () => {
               width={50}
               height={50}
             >
-              <line x1="6" y1={18} x2={18} y2={6}/>
-              <polyline points="9 6 18 6 18 15"/>
+              <line x1="6" y1={18} x2={18} y2={6} />
+              <polyline points="9 6 18 6 18 15" />
             </svg>
           </button>
-
         </Link>
       </div>
       {/* 博客分类列表 */}
-      <MainCategories/>
-      {/* 精选文章 */ }
-      <FeaturedPosts/>
+      <MainCategories />
+      {/* 精选文章 */}
+      <FeaturedPosts />
+      {/** 文章列表 */}
+      <div className="">
+        <h1 className="my-8 text-2xl text-gray-600">近期更新</h1>
+        <PostLists />
+      </div>
     </div>
   );
 };
