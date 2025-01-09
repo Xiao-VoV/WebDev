90476428935fcdf8e4756cdd276a09b25243e206
X.P <1679190840@qq.com>
2025-01-09 20:24:16 +0800

    添加评论组件和评论列表；在单篇文章页面中集成评论功能

--- src/routers/SinglePostPage.jsx
+++ src/routers/SinglePostPage.jsx
@@ -2,6 +2,7 @@ import Image from "../components/Image";
 import { Link } from "react-router-dom";
 import PostMenuActions from "../components/PostMenuActions";
 import Search from "../components/Search";
+import Comments from "../components/Comments";
 const SinglePostPage = () => {
   return (
     <div className="flex flex-col gap-8">
@@ -135,6 +136,7 @@ const SinglePostPage = () => {
           <Search />
         </div>
       </div>
+      <Comments />
     </div>
   );
 };
