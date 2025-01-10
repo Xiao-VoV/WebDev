c953729e60cb86408cf9e1aa99afbf2d56dcd18e
X.P <1679190840@qq.com>
2025-01-10 16:40:19 +0800

    添加文章列表页面和侧边菜单组件；更新路由配置以支持新页面

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,11 @@
+const SideMenu = () => {
+  return (
+    <div>
+      <h1 className="mb-4 text-sm font-medium">搜索</h1>
+      <h1 className="mt-8 mb-4 text-sm font-medium">过滤</h1>
+      <h1 className="mt-8 mb-4 text-sm font-medium">分类</h1>
+    </div>
+  );
+};
+
+export default SideMenu;
--- src/main.jsx
+++ src/main.jsx
@@ -1,65 +1,64 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-import Homepage from './routers/Homepage.jsx';
-import Write from './routers/Write.jsx';
-import LoginPage from './routers/LoginPage.jsx';
-import RegisterPage from './routers/Register.jsx';
-import SinglePostPage from './routers/SinglePostPage.jsx';
-import MainLayout from './layouts/MainLayout.jsx';
-import { ClerkProvider } from '@clerk/clerk-react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./routers/Homepage.jsx";
+import Write from "./routers/Write.jsx";
+import LoginPage from "./routers/LoginPage.jsx";
+import RegisterPage from "./routers/Register.jsx";
+import SinglePostPage from "./routers/SinglePostPage.jsx";
+import PostListPage from "./routers/PostListPage.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 // fr-FR locale is imported as frFR
-import { zhCN } from '@clerk/localizations'
+import { zhCN } from "@clerk/localizations";
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 //如果没有publishable key 就抛出错误
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
 const router = createBrowserRouter([
   {
-    element:<MainLayout/>,
-    children:[
+    element: <MainLayout />,
+    children: [
       {
         path: "/",
-        element:  (
-          <Homepage/>
-        ),
+        element: <Homepage />,
       },
       {
         path: "/:slug",
-        element: <SinglePostPage/>,
+        element: <SinglePostPage />,
       },
       {
         path: "/write",
-        element: <Write/>,
+        element: <Write />,
       },
       {
         path: "/login",
-        element: <LoginPage/>,
+        element: <LoginPage />,
       },
       {
         path: "/register",
-        element: <RegisterPage/>,
+        element: <RegisterPage />,
       },
-    ]
-  }
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+    ],
+  },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={zhCN}>
       <RouterProvider router={router} />
     </ClerkProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
